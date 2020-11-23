1e9553759dd7581b7d016aee8b7e6b8e9f2e77b0
wadwings <158802567@qq.com>
2020-11-23 20:32:12 +0800

    a lot of functions added

--- miniprogram/pages/collection/collection.js
+++ miniprogram/pages/collection/collection.js
@@ -1,29 +1,205 @@
+const app = getApp();
 Page({
     data: {
-
+        'record': null, //record = [{link, music, pic, ispermanent}]
+        'link': null,
+        'play': 0,
+        'playButtonPic':"../../images/play.png",
+        'count': 0,
+        'value': null,
+        'sharelink': null,
+        'pic': {
+            'AndroidPorn': "https://images.pexels.com/photos/3489391/pexels-photo-3489391.jpeg",
+        },
+        'currentTime': 0,
+        'duration': 1,
+        'currentPercent': 0
     },
     onLoad: function () {
+        this.f = swan.getFileSystemManager();
+        console.log(this.f);
+        this.setData({
+            'link': app.globalData.recorderHistorylink,
+            'music': app.globalData.recorderHistoryMusic
+        })
+        this.swipeInit();
+        console.log(this.data.record)
+        this.audioInit();
         // 监听页面加载的生命周期函数
     },
-    onReady: function() {
+    onReady: function () {
         // 监听页面初次渲染完成的生命周期函数
     },
-    onShow: function() {
+    onShow: function () {
         // 监听页面显示的生命周期函数
     },
-    onHide: function() {
+    onHide: function () {
         // 监听页面隐藏的生命周期函数
     },
-    onUnload: function() {
+    onUnload: function () {
         // 监听页面卸载的生命周期函数
     },
-    onPullDownRefresh: function() {
+    onPullDownRefresh: function () {
         // 监听用户下拉动作
     },
-    onReachBottom: function() {
+    onReachBottom: function () {
         // 页面上拉触底事件的处理函数
     },
     onShareAppMessage: function () {
         // 用户点击右上角转发
+    },
+    swipeInit() {
+        let arr = []
+        for (let i = 0; i < this.data.music.length; i++)
+            arr.push({
+                link: this.data.link[i],
+                music: this.data.music[i],
+                pic: this.data.pic[this.data.music[i]],
+                ispermanent: false,
+                value: null
+            })
+        this.setData({
+            record: arr
+        })
+    },
+    swipeChange(e){
+        this.setData({
+            playButtonPic: "../../images/play.png",
+            play: 0,
+            count: e.detail.current,
+            value: this.data.record[e.detail.current].value
+        })
+        this.audio.stop();
+        this.audioInit();
+    },
+    audioInit() {
+        if(this.audio){
+            this.audio.offTimeUpdate()
+            this.audio.destory()
+        }
+        const audio = swan.createInnerAudioContext();
+        this.audio = audio;
+        if (this.data.link) {
+            audio.src = this.data.link[this.data.count]
+            audio.onTimeUpdate(res => {
+                console.log(res.data.currentTime)
+                this.setData({
+                    'currentTime': res.data.currentTime,
+                    'duration': res.data.duration,
+                    'currentPercent': res.data.currentTime / res.data.duration * 100
+                })
+            }),
+            audio.onEnded(res =>{
+                this.audioStop()
+            })
+        }
+    },
+    sliderChanging() {
+        if(this.data.play)
+            this.audio.pause();
+    },
+    sliderChange(e) {
+        this.setData({
+            'currentTime': e.detail.value * this.data.duration / 100,
+            'currentPercent': e.detail.value
+        })
+        this.audio.seek(e.detail.value * this.data.duration / 100)
+        if(this.data.play)
+            this.audio.play();
+    },
+    playButtonPress() {
+        if (this.data.play === 0)
+            this.audioPlay();
+        else
+            this.audioPause();
+    },
+    audioPlay() {
+        this.audio.play();
+        this.setData({
+            play: 1,
+            playButtonPic: "../../images/pause.png"
+        })
+    },
+    audioStop(){
+        this.audio.stop();
+        this.setData({
+            play: 0,
+            playButtonPic: "../../images/play.png"
+        })
+    },
+    audioPause() {
+        this.audio.pause();
+        this.setData({
+            play: 0,
+            playButtonPic: "../../images/play.png"
+        })
+    },
+    renamefile(e) {
+        this.f.saveFile({
+            tempFilePath: this.data.link[this.data.count],
+            filePath: `${swan.env.USER_DATA_PATH}/${e.detail.value}.aac`,
+            success: res =>{
+                swan.showToast({
+                    title : "保存成功"
+                })
+                console.log(res.savedFilePath)
+                this.data.record[this.data.count].link = res.savedFilePath
+                this.data.record[this.data.count].permanent = true
+                this.data.record[this.data.count].value = e.detail.value
+            },
+            fail: err =>{
+                swan.showToast({
+                    title: '保存失败',
+                    icon: "none"
+                })
+            }
+        })
+    },
+    savefile() {
+        console.log(1)
+        this.f.saveFile({
+            tempFilePath: this.data.link[this.data.count],
+            success: res =>{
+                swan.showToast({
+                    title : "保存成功"
+                })
+                console.log(res.savedFilePath)
+                this.data.record[this.data.count].link = res.savedFilePath
+                this.data.record[this.data.count].permanent = true
+            },
+            fail: err =>{
+                swan.showToast({
+                    title: '保存失败'
+                })
+            }
+        })
+    },
+    share() {
+        if (!this.data.link) {
+            swan.showToast({
+                title: '录音文件不存在',
+                icon: 'none'
+            })
+        } else {
+            swan.shareFile({
+                filePath: this.data.record[this.data.count].link,
+                success: res => {
+                    swan.showToast({
+                        title: '分享成功',
+                        icon: 'none'
+                    })
+                },
+                fail: err => {
+                    swan.showToast({
+                        title: '分享失败',
+                        icon: 'none'
+                    })
+                }
+            })
+
+        }
+    },
+    navback() {
+        swan.navigateBack();
     }
 });
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,81 +1,250 @@
+const app = getApp();
 Page({
     data: {
+        'music': "AndroidPorn",
         'start': 0,
-        'audioSources': []
+        'audioSources': [],
+        'cdn': "https://unique-baiduprogram.cdn.bcebos.com/",
+        'extention': ".wav",
+        'audioFilePath': [],
+        'value': '',
+        "status" : "录制",
+        "arrow": "../../images/down.png",
+        'list': [{
+            'name': 'LimitLess',
+            "extention": '.mp3'
+        },{
+            'name': 'AndroidPorn',
+            "extention": '.wav'
+        }],
+        'select': 0
     },
     onLoad: function () {
-        swan.navigateTo({
-            url: '../collection/collection'
-        })
+        let that = this;
+        swan.setInnerAudioOption({
+            mixWithOther: true,
+            fail: err => {
+                swan.showModal({
+                    title: '设置与其他音频混播失败',
+                    content: JSON.stringify(err)
+                });
+                console.log('setInnerAudioOption fail', err);
+            }
+        });
+        this.recorderManagerInit(that);
+        //this.loadAudioFile();
+        // swan.navigateTo({
+        //     url: '../collection/collection'
+        // })
         // 监听页面加载的生命周期函数
+
     },
-    onReady: function() {
+    onReady: function () {
         // 监听页面初次渲染完成的生命周期函数
     },
-    onShow: function() {
+    onShow: function () {
         // 监听页面显示的生命周期函数
     },
-    onHide: function() {
+    onHide: function () {
         // 监听页面隐藏的生命周期函数
     },
-    onUnload: function() {
+    onUnload: function () {
         // 监听页面卸载的生命周期函数
     },
-    onPullDownRefresh: function() {
+    onPullDownRefresh: function () {
         // 监听用户下拉动作
     },
-    onReachBottom: function() {
+    onReachBottom: function () {
         // 页面上拉触底事件的处理函数
     },
     onShareAppMessage: function () {
         // 用户点击右上角转发
     },
-    recorder(){
+    recorder() {
         let that = this;
-        if(this.data.start)
+        if (this.data.start)
             this.recorderManagerStop(that)
         else
             this.recorderManagerStart(that)
     },
-    recorderManagerStart(that){
+    recorderManagerInit(that){
+        app.globalData.recorderHistorylink = []
+        app.globalData.recorderHistoryMusic = []
         swan.getAvailableAudioSources({
-            success: res =>{
+            success: res => {
                 let arr = []
-                for(let i = 1; i < res.audioSources.split('"').length;i += 2)
+                for (let i = 1; i < res.audioSources.split('"').length; i += 2)
                     arr.push(res.audioSources.split('"')[i])
                 that.setData({
                     'audioSources': arr
                 });
                 console.log('当前支持的音频输入源:', res.audioSources);
             },
-            fail: res =>{
+            fail: res => {
                 console.log('错误码: ' + err.errCode);
-                console.log('错误信息: '+ err.errMsg);
+                console.log('错误信息: ' + err.errMsg);
             }
         })
         const recorderManager = swan.getRecorderManager();
+        that.recorderManager = recorderManager;
+        console.log(that.recorderManager);
+        recorderManager.onStop(
+            function(res) {
+                console.log("record stop")
+                swan.showModal({
+                    title:"success",
+                    content: res.tempFilePath
+                })
+                app.globalData.recorderHistorylink.push(res.tempFilePath);
+                app.globalData.recorderHistoryMusic.push(that.data.music);
+            }
+        )
+
+    },
+    recorderManagerStart() {
         const options = {
             duration: 50000,
             sampleRate: 44100,
             numberOfChannels: 1,
             encodeBitRate: 96000,
-            audioSource: that.data.audioSources.indexOf('camcorder') ? that.data.audioSources[that.data.audioSources.indexOf('camcorder')] : 'auto'
+            audioSource: this.data.audioSources.indexOf('camcorder') ? this.data.audioSources[this.data.audioSources.indexOf('camcorder')] : 'auto'
         };
-        recorderManager.start(options);
-        this.recorderManager = recorderManager;
+        this.recorderManager.start(options);
         this.setData({
+            status: "停止",
             start: 1
         })
     },
-    recorderManagerStop(that){
+    recorderManagerStop() {
         this.recorderManager.stop();
         this.setData({
-            start: 0
+            start: 0,
+            status: "录制"
         })
     },
-    navigateTo(e){
+    navigateTo(e) {
         swan.navigateTo({
             url: '../collection/collection'
         })
+    },
+    loadAudioFile() {
+        this.data.audioFilePath = []
+        for (let i = 1; i < 19; i++)
+            swan.downloadFile({
+                url: this.data.cdn + this.data.music + '/' + this.data.music + '_chip' + i + this.data.extention,
+                success: res => {
+                    this.data.audioFilePath[i] = res.tempFilePath;
+                    console.log(res.tempFilePath);
+                },
+                fail: err => {
+                    this.toast('下载文件失败');
+                    console.log('错误码: ' + err.errCode);
+                    console.log('错误信息: ' + err.errMsg);
+                }
+            })
+    },
+    play1() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[1];
+        inneraudiocontext.play();
+    },
+    play2() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[2];
+        inneraudiocontext.play();
+    },
+    play3() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[3];
+        inneraudiocontext.play();
+    },
+    play4() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[4];
+        inneraudiocontext.play();
+    },
+    play5() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[5];
+        inneraudiocontext.play();
+    },
+    play6() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[6];
+        inneraudiocontext.play();
+    },
+    play7() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[7];
+        inneraudiocontext.play();
+    },
+    play8() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[8];
+        inneraudiocontext.play();
+    },
+    play9() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[9];
+        inneraudiocontext.play();
+    },
+    play10() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[10];
+        inneraudiocontext.play();
+    },
+    play11() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[11];
+        inneraudiocontext.play();
+    },
+    play12() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[12];
+        inneraudiocontext.play();
+    },
+    play13() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[13];
+        inneraudiocontext.play();
+    },
+    play14() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[14];
+        inneraudiocontext.play();
+    },
+    play15() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[15];
+        inneraudiocontext.play();
+    },
+    play16() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[16];
+        inneraudiocontext.play();
+    },
+    play17() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[17];
+        inneraudiocontext.play();
+    },
+    play18() {
+        let inneraudiocontext = swan.createInnerAudioContext();
+        inneraudiocontext.src = this.data.audioFilePath[18];
+        inneraudiocontext.play();
+    },
+    selectMusic(){
+        if(!this.data.select){
+            this.setData({
+                arrow: "../../images/up.png",
+                select: 1
+            })
+        }
+        else{
+            this.setData({
+                arrow: "../../images/down.png",
+                select: 0
+            })
+        }
     }
 });
